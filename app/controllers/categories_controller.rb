e28b6f200a4d4d484ebc1be596b30a021c1e34d4
Allan <amaxwellblair@gmail.com>
2016-02-29 21:26:54 -0700

    Style navbar, footer, cat index and categories
    Created partials for iteration of cats
    Drop down for filtering of cats by category
    Color scheme based on Compatible by joy_of_summer
    Edited profile picture placement, description and age in cat profiles

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,6 @@
 class CategoriesController < ApplicationController
   def show
+    @categories = Category.all
     @category = Category.find_by(name: params[:name])
     @cats = @category.cats
   end

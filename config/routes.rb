b065a26498fe2168a7d49a053cf0c12a8f8c6959
Allan <amaxwellblair@gmail.com>
2016-03-01 11:57:02 -0700

    Style landing page, navbar and each_cat

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  root to: "landing#show"
+
   resources :cats, only: [:index]
 
   resources :categories, only: [:show], param: :name
+
   resources :carts, only: [:create]
   get "/cart", to: "carts#show"
 end

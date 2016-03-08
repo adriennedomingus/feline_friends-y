1eeee4f03531af0233dcc55360aa3905d435309b
Adrienne Domingus <adrienne.domingus@gmail.com>
2016-03-08 15:43:21 -0700

    hound

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,7 +7,7 @@ class OrdersController < ApplicationController
   def create
     order = current_user.orders.new
     message = OrderProcessor.
-      process(order, @cart, start_date_params, end_date_params)
+              process(order, @cart, start_date_params, end_date_params)
     if message
       flash[:notice] = message
       redirect_to cart_path
